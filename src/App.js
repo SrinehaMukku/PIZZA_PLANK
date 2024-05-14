a910832a3c3958723dd71ec4a1815eabe9b76acf
Srineha_mukku <mukkusrineha.com>
2024-05-15 00:38:06 +0530

    css updated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
 function App() {
   return (
     <div className="App">
@@ -16,6 +18,8 @@ function App() {
         <Routes>
           <Route path="/" element = {<Home/>} />
           <Route path="/menu" element = {<Menu/>} />
+          <Route path="/about" element = {<About/>} />
+          <Route path="/contact" element = {<Contact/>} />
         </Routes>
         <Footer/>
       </Router>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PizzaLeft from "../assets/pizzaLeft.jpg";
+import PizzaLeft from "../assets/pepperoni.jpg";
 import "../styles/Contact.css";
 
 function Contact() {
