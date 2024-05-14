01f412b222b0b26cb3fa9d1c4c7ed9578cd393cf
Srineha_mukku <mukkusrineha.com>
2024-05-14 23:29:55 +0530

    new change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import Logo from '../assets/gamecard.png';
-import { Link } from 'react-router-dom';
-import "../styles/Navbar.css"
+import React, { useState } from "react";
+import Logo from "../assets/gamecard.png";
+import { Link } from "react-router-dom";
+import ReorderIcon from "@material-ui/icons";
+import "../styles/Navbar.css";
+
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
   return (
     <div className="navbar">
-       <div className="leftside">
-            <img src = {Logo} alt="Game card Logo"/>
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} alt="game card " />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
         </div>
-
-        <div className="rightside">
-            <Link to="/">Home</Link>
-            <Link to="/menu">Menu</Link>
-            <Link to="/about">About us</Link>
-            <Link to="/contact">Contact</Link>
-        </div>
-
+      </div>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
+      </div>
     </div>
   );
 }
